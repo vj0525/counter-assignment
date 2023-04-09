5bd5b51f2f8363835de7615f4d08792f6c501480
vj0525 <vjanapati05@gmail.com>
2023-04-09 14:03:17 -0400

    Unit Test code for counter assignment

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const countMessage = screen.getByText(/count/i);
+  expect(countMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId("count");
+  expect(countElement.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const increment = screen.getByText(/\+/i);
+  const countElement = screen.getByTestId("count");
+  fireEvent.click(increment);
+  expect(countElement.textContent).toBe("1");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const decrement = screen.getByText(/\-/i);
+  const countElement = screen.getByTestId("count");
+  fireEvent.click(decrement);
+  expect(countElement.textContent).toBe("-1");
 });
